08db199e8f7419ad6df59be69ebb52917710050e
philbarton <philbart@mac.com>
2019-05-16 16:41:05 +0100

    tidy up

--- src/main/java/adapter/CLIChangeEvent.java
+++ src/main/java/adapter/CLIChangeEvent.java
@@ -11,7 +11,7 @@ public class CLIChangeEvent {
 
     public static void main(String[] args) {
         UUID id = UUID.randomUUID();
-        String type = "bad";
+        String type = "create";
         String content = "some content";
 
         EventOrigin origin = new EventOrigin(id, type, content);
